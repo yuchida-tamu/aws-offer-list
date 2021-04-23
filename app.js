18e2f175e9cdd68025c99c3c496c3bbbfd358fad
yuchida-tamu <yutaside1992@gmail.com>
2021-04-23 22:58:35 +0900

    use inquirer, made it more interactive

--- app.js
+++ app.js
@@ -1,93 +1,95 @@
 const readline = require("readline");
+const inquirer = require("inquirer");
+const regionOptions = require("./regions");
 const axios = require("axios");
 let region, awsService, instanceType, operation;
 region = "ap-northeast-1";
 awsService = "AmazonRDS";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let queryParams = {
+  region: "ap-northeast-1",
+  service: "AmazonRDS",
+};
 
 const api = axios.create({
   baseURL: `https://pricing.us-east-1.amazonaws.com/`,
 });
 
-rl.question("Choose Your region (ap-northeast, us-east-1...): ", (answer) => {
-  region = answer;
-  rl.question("Choose AWS Service (AmazonEC2, AmazonRDS...): ", (answer) => {
-    awsService = answer;
-    rl.question(
-      "Choose instance type (m5.large, db.t3.xlarge etc...): ",
-      (answer) => {
-        instanceType = answer;
-        rl.question(
-          "Choose operation (CreateDBInstance:0015 etc...): ",
-          (answer) => {
-            operation = answer;
-            Promise.resolve()
-              .then(() => {
-                return api.get(
-                  `/offers/v1.0/aws/${awsService}/current/region_index.json`
-                );
-              })
-              .then((response) => {
-                const currentVersionUrl =
-                  response.data.regions[region].currentVersionUrl;
-                return api.get(currentVersionUrl);
-              })
-              .then((result) => {
-                const products = result.data.products;
-                const answer = [];
+//create questions
 
-                for (key in products) {
-                  let product = products[key];
-                  if (
-                    product.attributes.instanceType === instanceType &&
-                    product.attributes.operation === operation
-                  ) {
-                    answer.push(product);
-                  }
-                }
-                console.log(answer);
-              })
-              .catch((err) => {
-                console.log(err);
-                process.exit(1);
-              });
-            rl.close();
-          }
-        );
-      }
+const questionRegion = {
+  type: "list",
+  name: "region",
+  message: "リージョンを選んでください。Please, pick a region",
+  choices: regionOptions,
+};
+
+const questionService = {
+  type: "list",
+  name: "service",
+  message: "サービスを選んでください。Please, pick a service",
+  choices: ["AmazonRDS", "AmazonEC2"], //TODO increase services, export it out to make it more manageable
+};
+
+const questionInstance = {
+  name: "instance",
+  message:
+    "インスタンスタイプを入力してください。Please, enter an instance type",
+};
+
+const questionOperation = {
+  name: "operation",
+  message:
+    "オペレーション名を入力してください。　Please, enter an operation name",
+};
+
+inquirer
+  .prompt([
+    questionRegion,
+    questionService,
+    questionInstance,
+    questionOperation,
+  ])
+  .then((answers) => {
+    checkAPI(
+      answers.service,
+      answers.region,
+      answers.operation,
+      answers.instance
     );
+  })
+  .catch((err) => {
+    console.error("Something went wrong: ", err);
   });
-});
 
-// Promise.resolve()
-//   .then(() => {
-//     return api.get(`/offers/v1.0/aws/${awsService}/current/region_index.json`);
-//   })
-//   .then((response) => {
-//     const currentVersionUrl = response.data.regions[region].currentVersionUrl;
-//     return api.get(currentVersionUrl);
-//   })
-//   .then((result) => {
-//     const products = result.data.products;
-//     const answer = [];
+function checkAPI(awsService, region, operation, instanceType) {
+  Promise.resolve()
+    .then(() => {
+      return api.get(
+        `/offers/v1.0/aws/${awsService}/current/region_index.json`
+      );
+    })
+    .then((response) => {
+      const currentVersionUrl = response.data.regions[region].currentVersionUrl;
+      return api.get(currentVersionUrl);
+    })
+    .then((result) => {
+      const products = result.data.products;
+      const answer = [];
 
-//     for (key in products) {
-//       let product = products[key];
-//       if (
-//         product.attributes.instanceType === instanceType &&
-//         product.attributes.operation === operation
-//       ) {
-//         answer.push(product);
-//       }
-//     }
-
-//     console.log(answer);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     process.exit(1);
-//   });
+      for (key in products) {
+        let product = products[key];
+        if (
+          product.attributes.instanceType === instanceType &&
+          product.attributes.operation === operation
+        ) {
+          answer.push(product);
+        }
+      }
+      console.log(answer);
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
